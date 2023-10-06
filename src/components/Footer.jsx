fb84c61dda2af780fe50ceab2cd9cdfe0dd3552d
atipandde3898 <erpandeyatharv@gmail.com>
2023-10-06 11:49:14 +0530

    resume

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div class="text-center p-6 bg-black footer">
+        <span class="text-white">&copy; . </span>
+        <a class="link-underline text-white font-bold" href="https://rahulkarda.netlify.app">@ti pandey</a>
+      </div>
+    )
+}
+
+
+export default Footer
